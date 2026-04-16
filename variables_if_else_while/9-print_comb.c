0bf50d8bc508457647e4031ea792f8a261fb3790
DV1J <dvijpurmanun1@gmail.com>
2026-04-16 10:21:06 +0000

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,33 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
- *
- * Return: Always 0 (Success)
- */
+ 
+main - prints all single-digit numbers separated by comma and space*
+Return: Always 0 (Success)*/
 int main(void)
 {
-int i, j;
+    int i;
 
-for (i = 0; i <= 8; i++)
-{
-for (j = i + 1; j <= 9; j++)
-{
-putchar(i + '0');
-putchar(',');
-putchar(' ');
-putchar(j + '0');
-
-if (!(i == 8 && j == 9))
-{
-putchar(',');
-putchar(' ');
-
-}
-
-}
-
+    for (i = '0'; i <= '9'; i++)
+    {
+        putchar(i);
+        if (i != '9')
+        {
+            putchar(',');
+            putchar(' ');
+        }
 }
 putchar('\n');
+
 return (0);
 }
