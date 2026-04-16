5eecd462e27519a57dab29593a097a2ef7aeebf1
DV1J <dvijpurmanun1@gmail.com>
2026-04-16 10:24:07 +0000

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- 
-main - prints all single-digit numbers separated by comma and space*
-Return: Always 0 (Success)*/
+ *main - prints all single-digit numbers separated by comma and space
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i;
-
-    for (i = '0'; i <= '9'; i++)
-    {
-        putchar(i);
-        if (i != '9')
-        {
-            putchar(',');
-            putchar(' ');
-        }
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 
