330b87c6f090d10bba58c592e3aa55d91d95b18c
DV1J <dvijpurmanun1@gmail.com>
2026-04-16 06:07:51 +0000

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,11 +13,11 @@ for (letter = 'a'; letter <= 'z'; letter++)
 {
 putchar (letter);
 }
-for (letter = 'A'; lateer <= 'Z'; letter++)
+for (letter = 'A'; letter <= 'Z'; letter++)
 {
 putchar (letter);
 }
-putchar ('/n');
+putchar ('\n');
 
 return (0);
 }
