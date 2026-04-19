509f34c6c8601beb78f5d9bec58828bb8018e6bb
DV1J <dvijpurmanun1@gmail.com>
2026-04-19 18:23:25 +0000

    task 8

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int print_last_digit(int);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
 
 #endif
