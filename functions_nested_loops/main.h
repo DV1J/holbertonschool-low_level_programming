52a4f0779c62d6b22ffdb44a8b324ea32c3b6540
DV1J <dvijpurmanun1@gmail.com>
2026-04-19 18:29:56 +0000

    task 10

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,9 +5,10 @@ int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
 int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
 
 #endif
