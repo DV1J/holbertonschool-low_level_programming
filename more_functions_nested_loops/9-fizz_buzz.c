33c02eaba3c4137f4fef6f3fcf2026a147a34c3d
DV1J <dvijpurmanun1@gmail.com>
2026-04-21 09:49:39 +0000

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,16 +1,39 @@
-# FizzBuzz Test Solution
-# Iterate numbers from 1 to 100
-for num in range(1, 101):
-    # Check for multiples of both 3 and 5 first
-    if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz", end=" ")
-    # Check for multiples of 3
-    elif num % 3 == 0:
-        print("Fizz", end=" ")
-    # Check for multiples of 5
-    elif num % 5 == 0:
-        print("Buzz", end=" ")
-    # Print the number itself if not divisible by 3 or 5
-    else:
-        print(num, end=" ")
+#include <stdio.h>
 
+/**
+ * main - prints numbers from 1 to 100 with FizzBuzz rules
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
