473d1a95ca787499a023bf48394cd4ffe569fdb8
dv1j <dvijpurmanun1@gmail.com>
2026-05-03 14:36:33 +0400

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - prints lowercase alphabet in reverse
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
