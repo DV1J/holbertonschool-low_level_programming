10a210df4e9ae980dd5235d9b180e65f08ba9cd5
dv1j <dvijpurmanun1@gmail.com>
2026-05-03 12:08:37 +0400

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - prints lowercase then uppercase alphabet followed by a new line
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	for (alp = 'A'; alp <= 'Z'; alp++)
+	{
+		putchar(alp);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
