04a3fee55dabebcfd670a4757098020ea9918131
dv1j <dvijpurmanun1@gmail.com>
2026-05-03 13:49:45 +0400

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - prints single digits numbers of base 10 form 0
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int di;
+
+	for (di = 0; di < 10; di++)
+	{
+		printf("%d", di);
+	}
+
+	printf("\n");
+
+	return (0);
+}
