b47a1fa07b1db18c970c5a322154e7734092a388
dv1j <dvijpurmanun1@gmail.com>
2026-05-04 11:58:08 +0400

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *alhabet_x10 - prints alphabet in lowercase 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	char alp;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+	}
+	_putchar('\n');
+}
+
