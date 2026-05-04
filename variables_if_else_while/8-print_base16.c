8f83c1c23444b8fc743aa7becb478fae1499c573
dv1j <dvijpurmanun1@gmail.com>
2026-05-04 09:48:30 +0400

    task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n =0; n < 16; n++)
+	for (n = 0; n < 16; n++)
 	{
 		if (n < 10)
 		{
@@ -17,7 +17,7 @@ int main(void)
 		}
 		else if (n >= 10)
 		{
-			putchar (n + 'a');
+			putchar (n - 10 + 'a');
 		}
 	}
 	putchar ('\n');
