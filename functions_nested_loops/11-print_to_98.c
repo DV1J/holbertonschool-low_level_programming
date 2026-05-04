96dd0bc808875d8e82b4bb3c9a90467ff7c03d70
dv1j <dvijpurmanun1@gmail.com>
2026-05-05 03:36:37 +0400

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from input to 98,
- *          
- * @n: number counting starts at 
+ *print_to_98 - Prints all natural numbers from input to 98.
+ *
+ *@n: number counting starts at
  */
 void print_to_98(int n)
 {
