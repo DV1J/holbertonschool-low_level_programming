b3178ae8c7b5f5d2017f733ad3912d4d678c2b34
dv1j <dvijpurmanun1@gmail.com>
2026-05-04 11:38:27 +0400

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - it is a prototype
+ *
+ *Return: Alway 0
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+
+}
