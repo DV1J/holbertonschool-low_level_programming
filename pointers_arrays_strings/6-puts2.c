43816ac38ce11d154b4acce35ebef972465dde90
dv1j <dvijpurmanun1@gmail.com>
2026-05-10 14:35:21 +0400

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other character of a string
+ *@str: pointer where characters are located
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		if (n % 2 == 0)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
