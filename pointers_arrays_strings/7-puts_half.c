ffe604873ac12cca59406e6cb9a3de7288d2c958
dv1j <dvijpurmanun1@gmail.com>
2026-05-10 17:53:54 +0400

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *puts_half -  prints half of a string, followed by a new line.
+ *@str: contains string
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i, n;
+
+	for (len = 0; str[len] != '\0';len++)
+	{
+		/** calculating lenght */
+	}
+	n = (len + 1) / 2;
+	for (i = len - n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
