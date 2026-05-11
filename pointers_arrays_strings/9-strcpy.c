7c6f73fcaf93f333c1a27dda5227459de69f79c8
dv1j <dvijpurmanun1@gmail.com>
2026-05-11 08:45:20 +0400

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *9-strcpy.c - copies the string pointed to by src to dest.
+ *@dest: The destination buffer.
+ *@src: The source string to copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
