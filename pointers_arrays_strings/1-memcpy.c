56149d583480224653529e0327a3b9ba7d9da486
dv1j <dvijpurmanun1@gmail.com>
2026-05-18 01:39:06 +0400

    task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -5,18 +5,16 @@
  *@dest: where copied values will be stored
  *@src: pointer to n
  *@n: pointer where values are stored
- *return: dest
+ *Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char tmp;
 
 	for (i = 0; i < n; i++)
 	{
-		tmp = *src + 1;
 		dest[i] = *src;
-		*src = tmp;
+		*src = *src + 1;
 	}
 	return (dest);
 }
