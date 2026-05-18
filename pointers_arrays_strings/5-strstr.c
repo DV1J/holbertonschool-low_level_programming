efde26721277c1324d86890cad2b6e12f6eac936
dv1j <dvijpurmanun1@gmail.com>
2026-05-18 04:33:25 +0400

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: contains main string.
+ * @needle: is the substring.
+ * Return: matchs or is 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int	j = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
+
+	return (0);
+}
+
