eeeebd72d253cac6258129e9109b2266d77ab711
dv1j <dvijpurmanun1@gmail.com>
2026-05-19 07:22:29 +0400

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *main - multipiles two numbers
+ *@argc: contains arguments
+ *@argv: array to arguments
+ *Return: mul || 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
+
