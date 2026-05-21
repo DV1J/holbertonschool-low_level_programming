329c237121ca0c0ed002f5c865698839f6f23bb5
dv1j <dvijpurmanun1@gmail.com>
2026-05-21 10:43:09 +0400

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *str_concat - concatenates two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: null || arry
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	int len1 = 0, len2 = 0;
+	char *arry;
+
+	if (s1 == 0)
+	{
+		return (0);
+	}
+	if (s2 == 0)
+	{
+		return (0);
+	}
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	arry = malloc((len1 + len2 + 1) * sizeof(char));
+	if (arry == 0)
+	{
+		return (0);
+	}
+	while (i < len1)
+	{
+		arry[i] = s1[i];
+		i++;
+	}
+	while (j < len2)
+	{
+		arry[i + j] = s2[j];
+		j++;
+	}
+	arry[i + j] = '\0';
+	return (arry);
+	free(arry);
+}
