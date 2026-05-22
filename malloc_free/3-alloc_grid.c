fc69a674430d702a0957093cccaa0d889885c3ce
dv1j <dvijpurmanun1@gmail.com>
2026-05-22 11:37:57 +0400

    task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int col = 0, row = 0;
+	int col, row;
 	int **grid;
 
 	if (width <= 0 || height <= 0)
@@ -19,27 +19,27 @@ int **alloc_grid(int width, int height)
 	grid = malloc(height * sizeof(int *));
 	if (grid == 0)
 	{
-		free(grid);
 		return (0);
-	}
-	grid[0] = malloc(width * height * sizeof(int));
-	if (grid[0] == 0)
-	{
-		free(grid[0]);
-		return (0);
-	}
-	
-	for (row = 1; row < height; row++)
-	{
-		grid[row] = grid[0] + (row * width);
 	}
 	for (row = 0; row < height; row++)
 	{
-		col = 0;
-		while (col < width)
+		grid[row] = malloc(width * sizeof(int));
+		if (grid[row] == 0)
+		{
+			while (row > 0)
+			{
+				row--;
+				free(grid[row]);
+			}
+			free(grid);
+			return (0);
+		}
+	}
+	for (row = 0; row < height; row++)
+	{
+		for (col = 0; col < width; col++)
 		{
 			grid[row][col] = 0;
-			col++;
 		}
 	}
 	return (grid);
