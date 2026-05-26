904be46e187932a50c537db7b16a99472147ce14
dv1j <dvijpurmanun1@gmail.com>
2026-05-26 09:59:52 +0400

    task 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ *struct dog - what the dog doin
+ *@name: pointer that points to string for name
+ *@age: int of age
+ *@owner: pointer where string for owner is located
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
