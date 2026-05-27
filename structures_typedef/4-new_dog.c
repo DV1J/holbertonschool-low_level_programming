fefcc56519af5586b9f99ff6f39bfe6ededb41d4
dv1j <dvijpurmanun1@gmail.com>
2026-05-27 14:31:40 +0400

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ *new_dog - HOW MANY DOGS ARE THERE!?
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Return: NULL || newdog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog;
+
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == 0)
+		return (0);
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+	return (newdog);
+}
