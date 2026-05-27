20768653486742b1404c4a667c2477bc3567efb7
Dvij <dvijpurmanun1@gmail.com>
2026-05-27 19:33:36 +0400

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,15 +9,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+ dog_t *newdog2;
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == 0)
 	{
-		free(newdog);
 		return (0);
+ free(newdog);
 	}
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
+newdog2->name = newdog->name;
+newdog2->owner = newdog->owner;
+free(newdog);
 	return (newdog);
+
 }
