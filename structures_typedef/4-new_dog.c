83df6e53ab7ab2d48a6247c622f954e7bc7f55bd
dv1j <dvijpurmanun1@gmail.com>
2026-05-28 09:56:20 +0400

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,14 +9,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
- dog_t *newdog2;
+
+    dog_t *newdog2;
+
+	int nam_len = 0, own_len = 0;
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == 0)
+		return (0);
+
+	while (name[nam_len] != '\0')
 	{
+		nam_len++;
+	}
+	newdog->name = malloc((nam_len + 1) * (sizeof(dog_t)));
+	if (newdog->name == 0)
+	{
+		free(newdog->name);
 		return (0);
  free(newdog);
 	}
+
+	while (owner[own_len] != '\0')
+	{
+		own_len++;
+	}
+	newdog->owner = malloc((own_len + 1) * (sizeof(dog_t)));
+	if (newdog->owner == 0)
+	{
+		free(newdog->owner);
+		return (0);
+	}
+
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
@@ -25,4 +49,5 @@ newdog2->owner = newdog->owner;
 free(newdog);
 	return (newdog);
 
+	return (0);
 }
