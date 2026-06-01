b2b9e7effb930ad9a6f5325af0add76acf765137
dv1j <dvijpurmanun1@gmail.com>
2026-06-01 13:49:06 +0400

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,18 +9,17 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int prt = 0;
 
 	va_list print;
-	
+
 	va_start(print, n);
 
 	for (i = 0; i < n; i++)
 	{
-		prt += va_arg(print, unsigned int);
-		if (separator != NULL && i < n -1)
+		printf("%d", va_arg(print, unsigned int));
+		if (separator != NULL && i < (n - 1))
 		{
-			printf("%d%s", prt, separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
