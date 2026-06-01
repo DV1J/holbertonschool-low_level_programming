c929cf63e614a709dcd87c19f2c514f84dada3e2
dv1j <dvijpurmanun1@gmail.com>
2026-06-01 14:24:47 +0400

    task 2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ *print_strings - function that prints strings, followed by a new line.
+ *@separator: speparates the strings
+ *@n: contains string
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *prt_str;
+
+	va_list print;
+
+	va_start(print, n);
+
+	for (i = 0; i < n; i++)
+	{
+		prt_str = va_arg(print, char *);
+		if (prt_str == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", prt_str);
+		}
+		if (separator != NULL && (i + 1) < n)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(print);
+}
+
+
