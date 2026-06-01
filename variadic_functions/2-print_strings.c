fdc504ebe93623f2c8dba7d7e0d17adb3fc030d2
dv1j <dvijpurmanun1@gmail.com>
2026-06-01 14:30:43 +0400

    task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", prt_str);
 		}
-		if (separator != NULL && (i + 1) < n)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
