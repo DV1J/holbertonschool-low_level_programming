177a52ec4515439e49af6737c77f508a1906189e
dv1j <dvijpurmanun1@gmail.com>
2026-06-12 10:18:06 +0400

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *print_list - prints all the elements of a list_t list.
+ *@list_t:
+ *@h:
+ *Return: num of nodes 
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter;
+
+	for (counter = 0; h != NULL; counter++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return(counter);
+}
