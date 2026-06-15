443f8a32570d40dc1de67418337387a42055cf19
dv1j <dvijpurmanun1@gmail.com>
2026-06-15 14:50:51 +0400

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_dlistint - frees a dlistint_t list.
+ *@head: head pointer
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
